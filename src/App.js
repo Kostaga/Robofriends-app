6190a231d76808a2fa2aa16b598bbd0b605e7540
Konstantinos Agathopoulos <kostaga203@gmail.com>
2023-04-24 01:41:17 +0300

    Revert "optimized code and organized files"
    This reverts commit 0e4224ff2c66018bdd90a4641852b6a6adbd0f60.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox.js';
-import Scroll from '../components/Scroll';
+import CardList from './CardList';
+import SearchBox from './SearchBox.js';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -26,22 +26,27 @@ class App extends Component {
 	}
 
 	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
 
-		return !robots.length ? 
-			 <h1>Loading</h1> :	<div className='tc'>
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		}
+
+		else {
+
+			return (
+				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
 					<SearchBox searchChange = {this.onSearchChange} />
 					<Scroll>
 						<CardList robots={ filteredRobots } />
 					</Scroll>
 				</div>
-
+			);
 		}
-	
+	}
 	
 }
 
